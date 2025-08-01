af9d68ce824021ca6fa861fa9964ad4b2e537a74
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-08-01 22:03:25 +0530

    Made the Button Component Reusable using the Children and the Props

--- ProjectManagementApp/src/components/NoProjectSelected.jsx
+++ ProjectManagementApp/src/components/NoProjectSelected.jsx
@@ -1,9 +1,19 @@
+import noProject from "../assets/no-projects.png";
+import Button from "./Button.jsx";
+
 const NoProjectSelected = () => {
   return (
-    <div>
-      <img />
-      <h2>No Project Selected</h2>
-      <p> Select a Project or get started with a new one </p>
+    <div className="mt-24 text-center w-2/3">
+      <img src={noProject} className="w-16 h-16 object-contain mx-auto" />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-100 mb-4">
+        Select a Project or get started with a new one{" "}
+      </p>
+      <p>
+        <Button> Start a Project </Button>
+      </p>
     </div>
   );
 };
--- ProjectManagementApp/src/components/SideBar.jsx
+++ ProjectManagementApp/src/components/SideBar.jsx
@@ -1,16 +1,18 @@
+import Button from "../components/Button.jsx";
+
 const SideBar = () => {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-80rem rounded-r-xl">
-
-      <h1 className="mb-8 font-bold uppercase md:text-xl text-stone-200"> Your Projects</h1>
+      <h1 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        {" "}
+        Your Projects
+      </h1>
 
       <div>
-        <button className="px-4 py-2 text-xs md:text-base  rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">+ Add an new Project</button>
-
+        <Button>Add Project +</Button>
       </div>
-
     </aside>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
