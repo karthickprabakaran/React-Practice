f8908c345fc6cd21e4cca9a21c99149f3d11d9c5
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-08-01 21:35:55 +0530

    Added the Component to display when no Project is selected

--- ProjectManagementApp/src/App.jsx
+++ ProjectManagementApp/src/App.jsx
@@ -1,7 +1,6 @@
 import NewProject from "./components/NewProject";
 import SideBar from "./components/SideBar";
 
-
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
---
+++ ProjectManagementApp/src/components/NoProjectSelected.jsx
@@ -0,0 +1,11 @@
+const NoProjectSelected = () => {
+  return (
+    <div>
+      <img />
+      <h2>No Project Selected</h2>
+      <p> Select a Project or get started with a new one </p>
+    </div>
+  );
+};
+
+export default NoProjectSelected;
