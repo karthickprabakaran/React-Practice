3f84c3229b7ad00a2d768f3a29eee09c7b033ccd
karthickprabakaran <karthickprabakran06@gmail.com>
2025-08-07 18:55:35 +0530

    Added the close modal functionality

--- ProjectManagementApp/src/components/Modal.jsx
+++ ProjectManagementApp/src/components/Modal.jsx
@@ -1,5 +1,21 @@
-const Modal = (children) => {
-  return <dialog>{children}</dialog>;
-};
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = forwardRef(function Modal({ children }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => ({
+    open() {
+      if (dialog.current) {
+        dialog.current.showModal();
+      }
+    },
+  }));
+
+  return createPortal(
+    <dialog ref={dialog}>{children}</dialog>,
+    document.getElementById("modal-root"),
+  );
+});
 
 export default Modal;
--- ProjectManagementApp/src/components/NewProject.jsx
+++ ProjectManagementApp/src/components/NewProject.jsx
@@ -1,8 +1,9 @@
-import { useRef } from "react";
+import { useReducer, useRef } from "react";
 import Textinput from "./Textinput";
 import Modal from "./Modal.jsx";
 
 const NewProject = ({ onAdd, onCancel }) => {
+  const modal = useRef();
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -17,47 +18,52 @@ const NewProject = ({ onAdd, onCancel }) => {
       enteredDescription.trim() == "" ||
       enteredDate.trim() == ""
     ) {
+      console.log("the input is not complete");
       //show the error model
-      <Modal>
-        <h1>there is an error in the given date or description or the title</h1>
-      </Modal>;
+      modal.current.open();
+      return;
     } else {
       //push the details
       onAdd({
         title: enteredTitle,
-        description: enteredDate,
+        description: enteredDescription,
         dueDate: enteredDate,
       });
     }
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button
-            onClick={onCancel}
-            className="text-stone-800 hover:text-stone-950"
-          >
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleSave}
-            className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950"
-          >
-            {" "}
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Textinput type="text" ref={title} label="Title" />
-        <Textinput ref={description} label="Description" textarea={true} />
-        <Textinput type="date" ref={dueDate} label="Due Date" />
+    <>
+      <Modal ref={modal}>
+        <h2> Invalid Input Values Provided</h2>{" "}
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              onClick={onCancel}
+              className="text-stone-800 hover:text-stone-950"
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="px-6 py-2 bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              {" "}
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Textinput type="text" ref={title} label="Title" />
+          <Textinput ref={description} label="Description" textarea={true} />
+          <Textinput type="date" ref={dueDate} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
