415b0fd3f8a9e15ab9d816bf3345425650ee1bba
karthickprabakaran <karthickprabakran06@gmail.com>
2025-08-10 20:48:43 +0530

    Started with the count variable

--- react-BTS/index.js
+++ react-BTS/index.js
@@ -1,9 +1,32 @@
 
 
 
-const root = document.getElementById("text");
+function counter() {
+
+  let count = 0;
+
+  function increment() {
+    count++;
+  }
+
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("p", null, `Count : ${count} `),
+    React.createElement("button", { onClick: increment }, 'increment')
+  )
+}
 
 
+
+
+
+const rootElement = document.getElementById("root");
+
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(React.createElement(counter));
 
 
 
