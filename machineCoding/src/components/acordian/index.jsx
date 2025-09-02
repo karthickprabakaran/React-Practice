5a5b8602f0fb861f68f6a4e6bf3f668f23ab65bf
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-09-02 14:19:37 +0530

    Completed the single approach for the accordian with the single approach along with the closing

--- machineCoding/src/components/acordian/index.jsx
+++ machineCoding/src/components/acordian/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import data from "./data";
 
 
 export default function Accordian() {
@@ -7,10 +7,37 @@ export default function Accordian() {
   const [selected, setSelected] = useState(null);
 
 
+  function handleSingleSelect(getCurrentId) {
+
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+
+  }
+
   return <div className="wrapper">
 
     <div className="accordian">
 
+      {
+        data.length > 0 ?
+
+          data.map(item =>
+            <div className="item">
+              <div onClick={() => handleSingleSelect(item.id)} className="title">
+                <h3>{item.question}</h3>
+                <span>+</span>
+              </div>
+              {
+                selected === item.id ?
+                  <div className="content">{item.answer}</div>
+                  : null
+              }
+
+            </div>
+          )
+
+
+          : <h2>No Data is Found </h2>
+      }
 
     </div>
 
