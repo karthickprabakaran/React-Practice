f38c0ecde860c48c2e7bd6937f209761e303344f
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-09-02 16:56:46 +0530

    Completed the accordion

--- machineCoding/src/components/acordian/index.jsx
+++ machineCoding/src/components/acordian/index.jsx
@@ -1,47 +1,65 @@
 import { useState } from "react"
 import data from "./data";
+import "./styles.css";
 
 
 export default function Accordian() {
 
   const [selected, setSelected] = useState(null);
-
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
 
   function handleSingleSelect(getCurrentId) {
-
     setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+
+  function handleMultiSelection(getCurrentId) {
+    let cpyMul = [...multiple];
+
+    let index = cpyMul.indexOf(getCurrentId);
+
+    if (index === -1) {
+      cpyMul.push(getCurrentId);
+    } else {
+      cpyMul.splice(index, 1);
+    }
+
+    setMultiple(cpyMul);
 
   }
 
+  console.log(selected, multiple);
+
   return <div className="wrapper">
-
+    <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}> Enable Multi Selection</button>
     <div className="accordian">
-
       {
         data.length > 0 ?
-
           data.map(item =>
             <div className="item">
-              <div onClick={() => handleSingleSelect(item.id)} className="title">
+              <div onClick={
+
+                enableMultiSelection
+                  ?
+                  () => handleMultiSelection(item.id)
+                  :
+                  () => handleSingleSelect(item.id)} className="title">
                 <h3>{item.question}</h3>
                 <span>+</span>
               </div>
               {
-                selected === item.id ?
+                enableMultiSelection ?
+                  multiple.indexOf(item.id) !== -1 &&
                   <div className="content">{item.answer}</div>
-                  : null
+                  : selected === item.id && <div className="content">{item.answer}</div>
               }
-
             </div>
           )
-
-
           : <h2>No Data is Found </h2>
       }
-
     </div>
-
-  </div>
+  </div >
 
 
 }
