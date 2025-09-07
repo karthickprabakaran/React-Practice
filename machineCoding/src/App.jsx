def75c4b38aeea6a892241b806b6f26009d3038f
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-09-07 21:18:58 +0530

    Completed the star rating

--- machineCoding/src/App.jsx
+++ machineCoding/src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import Accordian from './components/acordian'
 import { RandomColour } from './components/randomColour'
+import { StarRating } from './components/StarRating'
 
 function App() {
 
   return (
     <>
-      <RandomColour />
+      <StarRating />
     </>
   )
 }
