bb571a89557d656c39f3625d8d94a6a888838692
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-09-17 12:21:40 +0530

    completed the css for the url based image slider

--- machineCoding/src/App.jsx
+++ machineCoding/src/App.jsx
@@ -4,12 +4,13 @@ import Accordian from './components/acordian'
 import { RandomColour } from './components/randomColour'
 import { StarRating } from './components/StarRating'
 import { ImageSlider } from './components/imageSlider/ImageSlider.jsx'
+import { Image } from './components/imgSliderguided/Image.jsx'
 
 function App() {
 
   return (
     <>
-      <ImageSlider />
+      <Image url={'https://picsum.photos/v2/list/'} limit={5} page={1} />
     </>
   )
 }
